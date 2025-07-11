7fc0e964bbc8f7ba730702bfe7d7012338d5b687
Codeck18 <leonardocodeck@gmail.com>
2025-07-11 20:41:23 -0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,6 @@
+print(10,20,30, sep=" - ", end=" # ")
+print(50,60,70, sep=" + ", end='\n')
+print(80,90,100, sep=" * ", end= '\n')
+
+#comando  end="\n" serve para pular para linha debaixo.
+print('Douglas','Simone',sep=' * ', end=' # ') .__format__
